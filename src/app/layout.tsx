dee158e806fb8571d7038f84f30b571edb263a9b
KJongHwa <dblls0724@naver.com>
2025-02-04 21:01:11 +0900

    Chore: Next.js 14.2.3 초기 환경세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "RE:PLAY",
+  description: "방탈출 정보와 리뷰, 모임까지 한 번에 즐기는 놀이터",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
