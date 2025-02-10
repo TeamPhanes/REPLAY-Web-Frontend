4d4f6b74718895183cecb24e844017f7cbd1f1e1
KJongHwa <dblls0724@naver.com>
2025-02-10 14:23:01 +0900

    Fix: npm high 에러 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        brand : '#515151'
+        brand: '#515151',
+        social: {
+          kakao: '#FEE500',
+          google: '#FFFFFF',
+          naver: '#03C75A',
+        },
       },
       width: {
         xs: '475px',
@@ -17,9 +22,15 @@ const config: Config = {
         md: '768px',
         lg: '1024px',
         xl: '1280px',
-      }
+      },
     },
   },
+  safelist: [
+    'bg-social-kakao', // kakao 색상 클래스
+    'bg-social-google', // google 색상 클래스
+    'bg-social-naver', // naver 색상 클래스
+  ],
   plugins: [],
 };
+
 export default config;
