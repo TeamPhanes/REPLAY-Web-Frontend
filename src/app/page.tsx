e115c6f087525c2cf65a46acc085da157580a9f1
KJongHwa <dblls0724@naver.com>
2025-04-01 01:49:53 +0900

    Feat : 홈페이지 Layout 및 GNV 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import PageContainer from '../components/@shared/layout/PageContainer';
+import SearchBar from '../components/@shared/search/SearchBar';
+import RoutingMenu from '../components/homePage/RoutingMenu';
+import TopCarousel from '../components/homePage/TopCarousel';
 
 export default function HomePage() {
   return (
     <PageContainer>
-      <p>홈 페이지</p>
+      <SearchBar />
+      <RoutingMenu />
+      <TopCarousel />
     </PageContainer>
   );
 }
