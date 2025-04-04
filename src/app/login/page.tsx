21139deb67676defe71e581aa2b2ab751d78f682
rave189 <rave189@naver.com>
2025-04-04 21:47:03 +0900

    fix lint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,24 @@
-'use client';
+"use client";
 
-import classNames from 'classnames';
-import Image from 'next/image';
-import PageContainer from '@/components/@shared/layout/PageContainer';
-import Logo from '@/public/images/logo.png';
-import { easyLoginIcons } from '@/constants/login/easyLoginIcons';
+import classNames from "classnames";
+import Image from "next/image";
+import PageContainer from "@/components/@shared/layout/PageContainer";
+import Logo from "@/public/images/logo.png";
+import { easyLoginIcons } from "@/constants/login/easyLoginIcons";
 
-const REDIRECT_URI = process.env.oauth2_host
-const NAVER_CLIENT_ID = process.env.naver_client_id
-const GOOGLE_CLIENT_ID = process.env.google_client_id
-const GOOGLE_SCOPE = "email profile"
-const KAKAO_CLIENT_ID = process.env.kakao_client_id
+const REDIRECT_URI = process.env.oauth2_host;
+const NAVER_CLIENT_ID = process.env.naver_client_id;
+const GOOGLE_CLIENT_ID = process.env.google_client_id;
+const GOOGLE_SCOPE = "email profile";
+const KAKAO_CLIENT_ID = process.env.kakao_client_id;
 
 const handleNaverLogin = async () => {
   const state = Math.random().toString(36).substring(2, 15);
 
-  await fetch('http://localhost:8080/auth/state', {
-    method: 'POST',
-    headers: { 'Content-Type': 'text/plain' },
-    body: state,
+  await fetch("http://localhost:8080/auth/state", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: state
   });
 
   window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}`;
@@ -27,10 +27,10 @@ const handleNaverLogin = async () => {
 const handleGoogleLogin = async () => {
   const state = Math.random().toString(36).substring(2, 15);
 
-  await fetch('http://localhost:8080/auth/state', {
-    method: 'POST',
-    headers: { 'Content-Type': 'text/plain' },
-    body: state,
+  await fetch("http://localhost:8080/auth/state", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: state
   });
 
   window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${encodeURIComponent(
@@ -41,10 +41,10 @@ const handleGoogleLogin = async () => {
 const handleKakaoLogin = async () => {
   const state = Math.random().toString(36).substring(2, 15);
 
-  await fetch('http://localhost:8080/auth/state', {
-    method: 'POST',
-    headers: { 'Content-Type': 'text/plain' },
-    body: state,
+  await fetch("http://localhost:8080/auth/state", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: state
   });
 
   window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}`;
@@ -53,7 +53,8 @@ const handleKakaoLogin = async () => {
 export default function LoginPage() {
   return (
     <PageContainer>
-      <div className="mx-auto flex h-[400px] w-[448px] flex-col items-center rounded-xl border-2 border-white shadow-xl">
+      <div
+        className="mx-auto flex h-[400px] w-[448px] flex-col items-center rounded-xl border-2 border-white shadow-xl">
         <Image
           src={Logo}
           alt="서비스 로고"
@@ -80,13 +81,13 @@ export default function LoginPage() {
                 <button
                   type="button"
                   className={classNames(
-                    'flex h-14 w-14 items-center justify-center rounded-full',
+                    "flex h-14 w-14 items-center justify-center rounded-full",
                     icon.color
                   )}
                   onClick={
-                    key === 'naver'
+                    key === "naver"
                       ? handleNaverLogin
-                      : key === 'google'
+                      : key === "google"
                         ? handleGoogleLogin
                         : handleKakaoLogin
                   }
