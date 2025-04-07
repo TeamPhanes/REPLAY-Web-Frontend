057ae2a2dd8d76420e2cf23cdbf8ffe0a39d340a
KJongHwa <dblls0724@naver.com>
2025-04-07 19:35:28 +0900

    Feat : MyPage를 위한 Routing 지정

---
+++ src/app/mypage/achievements/page.tsx
@@ -0,0 +1,11 @@
+import PageContainer from '@/src/components/@shared/layout/PageContainer';
+import MyPageNav from '@/src/components/myPage/MyPageNav';
+
+export default function AllAchievementsPage() {
+  return (
+    <PageContainer>
+      <MyPageNav />
+      <p>모든 업적 페이지</p>
+    </PageContainer>
+  );
+}
---
+++ src/app/mypage/schedule/page.tsx
@@ -0,0 +1,11 @@
+import PageContainer from '@/src/components/@shared/layout/PageContainer';
+import MyPageNav from '@/src/components/myPage/MyPageNav';
+
+export default function MySchedulePage() {
+  return (
+    <PageContainer>
+      <MyPageNav />
+      <p>나의 일정 페이지</p>
+    </PageContainer>
+  );
+}
