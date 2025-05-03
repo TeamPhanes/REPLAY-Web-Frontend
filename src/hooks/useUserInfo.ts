29071f9b2c41c52ec18ef31408ed9539a055ddef
KJongHwa <dblls0724@naver.com>
2025-05-03 10:32:17 +0900

    Refactor : hasHydrated enabled 추가

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,14 +1,16 @@
 import { useAuthStore } from '@/store/authStore';
 import { useQuery } from '@tanstack/react-query';
 import { GetUser } from '@/axios/user';
+import { useHasHydrated } from '@/hooks/useHasHydrated';
 
 export const useUserInfo = () => {
+  const hasHydrated = useHasHydrated();
   const accessToken = useAuthStore((state) => state.accessToken);
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['userInfo', accessToken],
     queryFn: () => GetUser(),
-    enabled: !!accessToken,
+    enabled: !!accessToken && hasHydrated,
     retry: false,
     staleTime: 1000 * 60 * 5,
   });
