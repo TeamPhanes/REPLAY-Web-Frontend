571ee9d804aa3d95672b029d6fc1358a75e20be1
KJongHwa <dblls0724@naver.com>
2025-05-05 12:36:40 +0900

    Feat : mypage 구현 및 Loading 구현

--- src/components/myPage/home/CardContainer.tsx
+++ src/components/myPage/home/CardContainer.tsx
@@ -1,21 +1,30 @@
-import { mockUser } from '@/data/mockUser';
+'use client';
+
+import Loading from '@/components/@shared/loading/Loading';
 import AchievementBadge from '@/components/gatheringDetail/AchievementBadge';
 import CardBottomDate from '@/components/myPage/home/CardBottomDate';
 import CardContentContainer from '@/components/myPage/home/CardContentContainer';
 import MyPageModifyButton from '@/components/myPage/home/MyPageModifyButton';
+import { useHasHydrated } from '@/hooks/useHasHydrated';
+import { useUserInfo } from '@/hooks/useUserInfo';
 
 export default function CardContainer() {
+  const hasHydrated = useHasHydrated();
+  const { userInfo, isLoading } = useUserInfo();
+
+  if (!hasHydrated || isLoading) return <Loading isLoading={isLoading} />;
+
   return (
     <div className="relative mt-5 h-[983px] w-full rounded-[30px] bg-cardActive p-10">
       <MyPageModifyButton />
-      <CardContentContainer user={mockUser} />
+      <CardContentContainer user={userInfo} />
       <CardBottomDate
-        updatedAt={mockUser.updatedAt}
-        createdAt={mockUser.createdAt}
+        updatedAt={userInfo.updatedAt}
+        createdAt={userInfo.createdAt}
       />
       <AchievementBadge
-        nickname={mockUser.nickname}
-        achievement={mockUser.representAchievement}
+        nickname={userInfo.nickname}
+        achievement={userInfo.representAchievement}
         absoluteLayout="mt-9 bottom-2 right-10 gap-10"
         type="mypage"
       />
