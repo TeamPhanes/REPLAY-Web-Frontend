8e3e9956980ed426a0ba5dd17bceff956c719ade
KJongHwa <dblls0724@naver.com>
2025-05-19 18:43:42 +0900

    Fix : build 오류 해결

--- src/app/gathering/[id]/page.tsx
+++ src/app/gathering/[id]/page.tsx
@@ -27,7 +27,7 @@ export default function GatheringDetailPage() {
         list={findDetailGathering}
         detail={mockGatheringsDetail}
       />
-      <ParticipantList id={id} />
+      <ParticipantList />
       <CommentsContainer id={id} leaderCheck={mockGatheringsDetail.leader} />
       <AnotherGatherings
         title={`${mockGatheringsDetail.leader}님이 만든 모임`}
