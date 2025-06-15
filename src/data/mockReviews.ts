ecc1c2ae6c3fbb030ea7c35f33deed56f18a53bb
KJongHwa <dblls0724@naver.com>
2025-06-15 23:31:13 +0900

    Fix : build 에러 해결

--- src/data/mockReviews.ts
+++ src/data/mockReviews.ts
@@ -9,8 +9,9 @@ export const mockReviews: ReviewDTO['get'][] = [
     },
     content:
       '상상의문에서 해본 테마 : 직박구리 / 혼숨 / 컴퍼니 / 퍼펫쇼 인데, 공테나 스릴러 좋아하신다면 혼숨 추천하고, 커플끼리 하기 좋은건 직박구리와 추천이에요. 장치도 다양하고, 스토리도 탄탄하고 재밌어요 👍👍  제일 재미있던건 혼숨인데 처음이라면 직박구리 추천하구요. 조금 특이한거 하고 싶다 하시면 퍼펫쇼도 좋을 것 같아요 직원분들도 친절하고 좋았습니다 다음에 또 방문 할려구요. 조금 특이한거 있다면 방탈출에서 이런 경험하기 쉽지 않았을거 같아여',
-    image:
+    images: [
       'https://i.namu.wiki/i/z7daGwnyG4XZTW0ZtbTHwQUwR8vmFwqh3CEfk8lg32xdlwjJ37daboKgEdorUiKndajVRqLfTFFsEQC5qfyiOA.webp',
+    ],
     rating: 3,
     success: true,
     themeReview: '보통',
@@ -29,7 +30,7 @@ export const mockReviews: ReviewDTO['get'][] = [
     },
     content:
       '상상의문에서 해본 테마 : 직박구리 / 혼숨 / dkdk 컴퍼니 / 퍼펫쇼 인데, 공테나 스릴러 좋아하신다면 혼숨 추천하고, 커플끼리 하기 좋은건 직박구리와 dkdk 추천이에요. 장치도 다양하고, 스토리도 탄탄하고 재밌어요 👍👍  제일 재미있던건 혼숨인데 처음이라면 직박구리 추천하구요. 조금 특이한거 하고 싶다 하시면 퍼펫쇼도 좋을 것 같아요 직원분들도 친절하고 좋았습니다 다음에 또 방문 할려구요. 조금 특이한거 하고 싶다 하시면 퍼펫쇼도 좋을 것 같아요 직원분들도 친절하고 좋았습니다 다음에 또 방문 할려구요. ',
-    image: '',
+    images: [''],
     rating: 5,
     success: false,
     themeReview: '좋아요',
