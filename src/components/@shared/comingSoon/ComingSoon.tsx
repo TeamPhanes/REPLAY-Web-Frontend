4ba3f013f43c5b6b712c3bd30bdd177c539eff19
KJongHwa <dblls0724@naver.com>
2025-07-02 08:02:57 +0900

    Design : ComingSoon 디자이너님 요청 위치 조정

--- src/components/@shared/comingSoon/ComingSoon.tsx
+++ src/components/@shared/comingSoon/ComingSoon.tsx
@@ -4,28 +4,29 @@ import ComingSoonShadow from '@/public/images/comingSoon/comingSoon_shadow.png';
 
 export default function ComingSoon() {
   return (
-    <div className="flex justify-center items-center flex-col">
-      <p className="font-normal text-[64px] tracking-[0.31em] mb-40 max-w-[665px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white">
+    <div className="flex justify-between items-center flex-col h-[796px]">
+      <p className="font-normal text-[64px] tracking-[0.31em] mt-20 mb-20 max-w-[665px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white">
         COMING SOON
       </p>
-      <Image
-        src={ComingSoonDoor}
-        alt="우리"
-        width={271}
-        height={548}
-        priority
-        quality={100}
-        className=""
-      />
-      <Image
-        src={ComingSoonShadow}
-        alt="곧 만나요!"
-        width={747}
-        height={128}
-        priority
-        quality={100}
-        className="ml-6"
-      />
+      <div className="flex flex-col items-center absolute -bottom-32">
+        <Image
+          src={ComingSoonDoor}
+          alt="우리"
+          width={271}
+          height={548}
+          priority
+          quality={100}
+        />
+        <Image
+          src={ComingSoonShadow}
+          alt="곧 만나요!"
+          width={747}
+          height={128}
+          priority
+          quality={100}
+          className="ml-6"
+        />
+      </div>
     </div>
   );
 }
