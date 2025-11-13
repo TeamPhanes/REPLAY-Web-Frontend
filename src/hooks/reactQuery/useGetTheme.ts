653125f7e15c186b9f21b8abceaf69214c09e58b
KJongHwa <dblls0724@naver.com>
2025-11-13 18:26:24 +0900

    Feat : 장르 필터 통합으로 보내주되, city의 전체는 지우고 호출하도록 구현

--- src/hooks/reactQuery/useGetTheme.ts
+++ src/hooks/reactQuery/useGetTheme.ts
@@ -7,14 +7,17 @@ import { useShowLoading } from '@/hooks/useShowLoading';
 export const useGetTheme = (
   accessToken: string | null,
   state: string[],
-  city: string[],
   genres: string[],
   page: number,
   size: number
 ) => {
+  const filteredState = state.map((item) =>
+    item.endsWith(' 전체') ? item.replace(' 전체', '') : item
+  );
   const { data, isLoading, error } = useQuery({
-    queryKey: ['theme', accessToken, state, city, genres, page, size],
-    queryFn: () => GetTheme({ accessToken, state, city, genres, page, size }),
+    queryKey: ['theme', accessToken, state, genres, page, size],
+    queryFn: () =>
+      GetTheme({ accessToken, state: filteredState, genres, page, size }),
     retry: false,
     staleTime: 1000 * 60 * 5,
     placeholderData: keepPreviousData,
