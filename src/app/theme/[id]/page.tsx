21b93dd0e1f551b659f1c26806832a71aa9e37b1
KJongHwa <dblls0724@naver.com>
2025-11-16 18:08:53 +0900

    Feat : 방탈출 상세 페이지 리디자인 최종 적용 및 구현

--- src/app/theme/[id]/page.tsx
+++ src/app/theme/[id]/page.tsx
@@ -1,20 +1,64 @@
 'use client';
 
+import { useState } from 'react';
 import { useParams } from 'next/navigation';
+import { useAuthStore } from '@/store/authStore';
 import PageContainer from '@/components/@shared/layout/PageContainer';
-import RoomDetailAllRating from '@/components/roomDetail/RoomDetailAllRating';
+import Loading from '@/components/@shared/loading/Loading';
+import ReviewSection from '@/components/review/ReviewSection';
 import RoomDetailCard from '@/components/roomDetail/RoomDetailCard';
 import RoomDetailGatherings from '@/components/roomDetail/RoomDetailGatherings';
-import RoomDetailReviews from '@/components/roomDetail/RoomDetailReviews';
+import { useGetOtherGathering } from '@/hooks/reactQuery/useGetOtherGathering';
+import { useGetReview } from '@/hooks/reactQuery/useGetReview';
+import { useGetThemeDetail } from '@/hooks/reactQuery/useGetTheme';
+import { usePagination } from '@/hooks/usePagination';
 
 export default function RoomDetailPage() {
+  const [page, setPage] = useState(0);
   const { id } = useParams();
+  const { accessToken } = useAuthStore();
+  const { themeDetail, isLoading: themeDetailLoading } = useGetThemeDetail(id);
+  const { otherGathering, isLoading: otherGatheringLoading } =
+    useGetOtherGathering(accessToken, id);
+  const { review, isLoading: reviewLoading } = useGetReview(
+    accessToken,
+    id,
+    page,
+    10
+  );
+  const totalItems = review ? review.totalCount : 0;
+  const { totalPages } = usePagination(page, totalItems);
+
+  if (
+    themeDetailLoading ||
+    otherGatheringLoading ||
+    reviewLoading ||
+    !themeDetail ||
+    !otherGathering ||
+    !review
+  )
+    return (
+      <Loading
+        isLoading={themeDetailLoading || otherGatheringLoading || reviewLoading}
+      />
+    );
 
   return (
     <PageContainer>
-      <RoomDetailCard id={id} />
-      <RoomDetailGatherings id={id} />
-      <RoomDetailReviews id={id} />
+      <RoomDetailCard data={themeDetail} />
+      <RoomDetailGatherings data={otherGathering.content} />
+      <div className="flex items-center gap-2 mt-16">
+        <span className="w-1 h-[30px] bg-line-lightGray" />
+        <p className="text-[28px]/[38px] tracking-[-2.5%] text-font-baseWhite font-semibold">
+          리뷰
+        </p>
+      </div>
+      <ReviewSection
+        data={review}
+        page={page}
+        totalPages={totalPages}
+        setPage={setPage}
+      />
     </PageContainer>
   );
 }
