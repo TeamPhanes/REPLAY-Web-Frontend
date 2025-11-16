a87c16aa78b21ee1be7029c5e20fa1b07258c901
KJongHwa <dblls0724@naver.com>
2025-11-16 18:05:37 +0900

    Refactor : 좋아요 기능 API PATH 수정

--- src/axios/theme.ts
+++ src/axios/theme.ts
@@ -57,7 +57,7 @@ export const GetThemeDetail = async (id: string | string[]) => {
 
 export const PostLikeTheme = async (themeId: number) => {
   try {
-    await axiosInstance.post(`${API_PATH.theme.default}/${themeId}/like`);
+    await axiosInstance.post(`${API_PATH.theme.default}/like/${themeId}`);
   } catch (error) {
     toast.error('찜하기 진행 중 오류가 있습니다.');
     throw error;
@@ -66,7 +66,7 @@ export const PostLikeTheme = async (themeId: number) => {
 
 export const DeleteLikeTheme = async (themeId: number) => {
   try {
-    await axiosInstance.delete(`${API_PATH.theme.default}/${themeId}/like`);
+    await axiosInstance.delete(`${API_PATH.theme.default}/like/${themeId}`);
   } catch (error) {
     toast.error('찜하기 취소 중 오류가 있습니다.');
     throw error;
@@ -75,7 +75,7 @@ export const DeleteLikeTheme = async (themeId: number) => {
 
 export const PostMarkTheme = async (themeId: number) => {
   try {
-    await axiosInstance.post(`${API_PATH.theme.default}/${themeId}/visit`);
+    await axiosInstance.post(`${API_PATH.theme.default}/visit/${themeId}`);
   } catch (error) {
     toast.error('참여 목록 추가 중 오류가 있습니다.');
     throw error;
@@ -84,7 +84,7 @@ export const PostMarkTheme = async (themeId: number) => {
 
 export const DeleteMarkTheme = async (themeId: number) => {
   try {
-    await axiosInstance.delete(`${API_PATH.theme.default}/${themeId}/visit`);
+    await axiosInstance.delete(`${API_PATH.theme.default}/visit/${themeId}`);
   } catch (error) {
     toast.error('참여 목록 취소 중 오류가 있습니다.');
     throw error;
