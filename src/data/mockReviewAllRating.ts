ac9b80859d8220fe05e7a378d9361e49294ce82f
KJongHwa <dblls0724@naver.com>
2025-12-10 00:38:22 +0900

    Fix : build error fix

--- src/data/mockReviewAllRating.ts
+++ src/data/mockReviewAllRating.ts
@@ -1,6 +1,4 @@
-import { ReviewAllRatingDTO } from '@/types/review/review.type';
-
-export const mockReviewAllRating: ReviewAllRatingDTO['get'] = {
+export const mockReviewAllRating = {
   scoreCount: 1024,
   averageScore: 3.5,
   score: [200, 212, 512, 60, 40],
