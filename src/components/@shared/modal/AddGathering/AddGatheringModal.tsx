89abdfa0d5274166755c5640aca8e22376befcd8
KJongHwa <dblls0724@naver.com>
2026-02-01 18:34:25 +0900

    Design : 모달 생성 리뉴얼 디자인 구현

--- src/components/@shared/modal/AddGathering/AddGatheringModal.tsx
+++ src/components/@shared/modal/AddGathering/AddGatheringModal.tsx
@@ -10,6 +10,8 @@ import AddGatheringWrite from '@/components/@shared/modal/AddGathering/AddGather
 import Modal from '@/components/@shared/modal/Modal';
 import usePostGatheringForm from '@/hooks/form/usePostGatheringForm';
 import { usePostGathering } from '@/hooks/reactQuery/usePostGathering';
+import MainBlueButton from '../../button/MainBlueButton';
+import MainWhiteButton from '../../button/MainWhiteButton';
 
 interface AddGatheringModalProps {
   isOpen: boolean;
@@ -25,6 +27,7 @@ export default function AddGatheringModal({
   themeIdProps,
 }: AddGatheringModalProps) {
   const [search, setSearch] = useState(themeNameProps ?? '');
+  const [mobilePage, setMobilePage] = useState(0);
   const { mutate } = usePostGathering();
   const {
     register,
@@ -41,18 +44,26 @@ export default function AddGatheringModal({
     }
   }, [isOpen, themeIdProps, setValue]);
 
+  useEffect(() => {
+    if (isOpen) {
+      setMobilePage(0);
+    }
+  }, [isOpen]);
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      className="relative w-[1064px] rounded-lg bg-[#F7F7FB] p-8"
+      className="relative w-[1064px] rounded-lg bg-[#F7F7FB] p-4 xl:p-8"
     >
       <h2 className="mb-[60px] mt-7 text-center text-[28px]/[38px] font-semibold tracking-[-2.5%] text-font-baseBlack">
         모임 생성
       </h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex justify-between">
-          <div className="flex w-[454px] flex-col gap-2">
+        <div className="flex justify-center gap-5 md:justify-between">
+          <div
+            className={`${mobilePage !== 0 ? 'hidden' : ''} flex w-full flex-col justify-between gap-2 md:w-[454px]`}
+          >
             <p className="text-2xl/[34px] font-normal tracking-[-2.5%] text-font-baseBlack">
               모임 정보
             </p>
@@ -78,11 +89,13 @@ export default function AddGatheringModal({
             />
           </div>
 
-          <div className="flex w-[480px] flex-col gap-5">
+          <div
+            className={`${mobilePage !== 1 ? 'hidden md:flex' : ''} flex-col gap-5 md:w-[480px]`}
+          >
             <p className="text-2xl/[34px] font-normal tracking-[-2.5%] text-font-baseBlack">
               모임 상세
             </p>
-            <div className="flex justify-between">
+            <div className="mt-6 flex flex-col items-start justify-between gap-5 md:mt-0 xl:flex-row xl:gap-2">
               <AddGatheringCapacity
                 capacity={watch('capacity')}
                 capacityChange={(capacity) => setValue('capacity', capacity)}
@@ -105,9 +118,11 @@ export default function AddGatheringModal({
           </div>
         </div>
 
-        <div className="mt-20 flex justify-between">
-          <div className="flex w-[454px] flex-col gap-5">
-            <p className="text-2xl/[34px] font-normal tracking-[-2.5%] text-font-baseBlack">
+        <div className="flex justify-center gap-5 md:mt-20 md:justify-between">
+          <div
+            className={`${mobilePage !== 1 ? 'hidden md:flex' : ''} mt-10 shrink-0 flex-col gap-5 md:mt-0 md:w-full md:shrink`}
+          >
+            <p className="mb-6 text-2xl/[34px] font-normal tracking-[-2.5%] text-font-baseBlack md:mb-0">
               일정 선택
             </p>
             <AddGatheringRecruitmentPeriod
@@ -125,17 +140,42 @@ export default function AddGatheringModal({
               dateTimeChange={(dateTime) => setValue('dateTime', dateTime)}
             />
           </div>
-          <AddGatheringWrite
-            content={watch('content')}
-            contentChange={(content) =>
-              setValue('content', content, { shouldValidate: true })
-            }
-            register={register}
-            errors={errors}
-          />
+          <div
+            className={`${mobilePage !== 2 ? 'hidden md:flex' : ''} w-full flex-col gap-5 xl:w-[480px]`}
+          >
+            <AddGatheringWrite
+              content={watch('content')}
+              contentChange={(content) =>
+                setValue('content', content, { shouldValidate: true })
+              }
+              register={register}
+              errors={errors}
+            />
+          </div>
         </div>
-
-        <AddGatheringButton onClose={onClose} />
+        <div className="my-7 flex justify-center gap-2 md:hidden">
+          <MainWhiteButton
+            className={`${mobilePage === 0 ? 'hidden' : ''} min-w-40 !text-base`}
+            onClick={() => setMobilePage((prev) => prev - 1)}
+          >
+            이전으로
+          </MainWhiteButton>
+          <MainBlueButton
+            className={`${mobilePage === 2 ? 'hidden' : ''} min-w-40 !text-base`}
+            onClick={() => setMobilePage((prev) => prev + 1)}
+          >
+            다음으로
+          </MainBlueButton>
+          <MainBlueButton
+            className={`${mobilePage !== 2 ? 'hidden' : ''} min-w-40 !text-base`}
+            type="submit"
+          >
+            등록하기
+          </MainBlueButton>
+        </div>
+        <div className="mt-14 hidden justify-center gap-2 md:flex">
+          <AddGatheringButton onClose={onClose} />
+        </div>
       </form>
     </Modal>
   );
